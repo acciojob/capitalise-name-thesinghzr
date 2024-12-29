3387312e1e4fa0835de1a8db081ff56d8ea27644
Suraj Singh <yousurajsingh@gmail.com>
2024-12-29 21:08:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const input = document.querySelector("#fname")
+input.addEventListener("mouseleave",(e)=>{
+	let a = input.value
+	e.target.value = a.toUpperCase()
+	// console.log(e.target.value)
+})
